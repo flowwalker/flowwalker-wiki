6f7390362bc9c391c3be5ca849f206a8dd44ea54
flowwalker <2870541857@qq.com>
2026-07-21 13:22:05 +0800

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🚀flowwalker的悟道Wiki",
+    pageTitle: "🚀  flowwalker的悟道Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
